3863b5ece431441882d5cec6e5704ea5c2d41ab4
hongchingisme <hongchingisme@gmail.com>
2021-01-12 20:10:52 +0800

    Add Regular Expression function to Email Input

--- script.js
+++ script.js
@@ -16,3 +16,8 @@ function showSuccess(input){
 const formControl = input.parentElement;
 formControl.className = 'form-control success';
 }
+
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
