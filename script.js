7734d61b24d494d978f0f056fa97cc04cd71ee38
hongchingisme <hongchingisme@gmail.com>
2021-01-16 13:27:15 +0800

    modify rate value bug

--- script.js
+++ script.js
@@ -1,71 +1,45 @@
 
 
 function updateprice (){
-
 var CurrencyOne = $('#currency-one').val();
 var CurrencyTwo = $('#currency-two').val();
 var AmountOne = $("#amount-one").val();
-
-
-
-
-
     $.ajax({
-       
         url: `https://v6.exchangerate-api.com/v6/97f7cb6c49c8bfb9aceb3eda/latest/${CurrencyOne}`,
         method: "GET",
         dataType: "json",
-        success: function(re) { 
-          
-          const rate = re.conversion_rates[CurrencyTwo];
-          //console.log(rate);
-          $('#rate').append(`1 ${CurrencyOne} = ${rate} ${CurrencyTwo}`);
-          $("#amount-two").val((AmountOne * rate));
-
-
-        }
-
-    
-        
+        success: function(re) {  
+          const rate = re.conversion_rates[CurrencyTwo];         
+          $("#amount-two").val((AmountOne * rate).toFixed(2));
+          $('#rate').text(`1 ${CurrencyOne} = ${rate} ${CurrencyTwo}  `)
+        }       
       })
-
     }
 
     updateprice ()
     
     $("#currency-two").change(function (e) { 
-      e.preventDefault();
-      $("#rate").html("");
       updateprice ();
-      
     });
 
     $("#amount-two").change(function (e) { 
-      e.preventDefault();
-      $("#rate").html("");
       updateprice ();
-      
     });
 
     $("#amount-one").change(function (e) { 
-      e.preventDefault();
-      $("#rate").html("");
       updateprice ();
-      
     });
 
     $("#currency-one").change(function (e) {
-      e.preventDefault();
-      $("#rate").html("");
       updateprice ();
     })
 
     $('#swap').click(function (e) { 
-      $("#rate").html("");
       var temp =  $('#currency-one').val()
       $('#currency-one').val($('#currency-two').val())
       $('#currency-two').val(temp);
       updateprice ()
       
     });
-
+    
+    updateprice ()
