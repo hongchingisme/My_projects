02c4c8498fa1277360cd864aa24de49f6f548651
hongchingisme <hongchingisme@gmail.com>
2021-01-16 11:52:21 +0800

    change select2 can update value

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var CurrencyOne = $("#currency-one");
+function updateprice (){
+
+var CurrencyOne = $("#currency-one").val();
 var CurrencyTwo = $('#currency-two').val();
-var AmountOne = $("#amount-one");
-var AmountTwo = $("#amount-two");
+var AmountOne = $("#amount-one").val();
+var AmountTwo = $("#amount-two").val();
+var rateText = $('#rate') 
 
 
 
@@ -10,11 +13,22 @@ var AmountTwo = $("#amount-two");
         url: "https://v6.exchangerate-api.com/v6/97f7cb6c49c8bfb9aceb3eda/latest/USD",
         method: "GET",
         dataType: "json",
-        success: function(re) {
-            $("#rate").text(
-                re.conversion_rates[CurrencyTwo]
-                );
-            
+        success: function(re) { 
+          
+          var price = rateText.text(re.conversion_rates[CurrencyTwo]); 
+          price = AmountOne * price ;
+          AmountTwo = price ;
         }
+        
       })
 
+    }
+
+    updateprice ()
+    
+    $("#currency-two").change(function (e) { 
+      e.preventDefault();
+
+      updateprice ();
+      
+    });
