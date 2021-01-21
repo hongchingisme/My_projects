731e43f32af095fc8d546e46de1e0da42412f509
hongchingisme <hongchingisme@gmail.com>
2021-01-21 18:43:11 +0800

    Get DOM

---
+++ Expense Tracker/srcipt.js
@@ -0,0 +1,7 @@
+const balance = $('#balance');
+const money_plus = $('#money-plus');
+const money_minus = $('#money-minus');
+const list = $('#list');
+const form = $('#form');
+const text = $('#text');
+const amount = $('#amount');
