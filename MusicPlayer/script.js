447a40565b234ac161f1cc15603779ebe4cd4721
hongchingisme <hongchingisme@gmail.com>
2021-01-25 20:44:50 +0800

    Show song name

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -7,3 +7,24 @@ const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover =  document.getElementById('cover');
+
+
+// 歌曲的名稱
+
+const songs = ['HelloGoodbye' ,'Onemoretime' ,'RainMusic'] ;
+
+// 抓取歌曲位置
+
+let songIndex = 0;
+
+// 將歌曲的的資訊放進 DOM
+
+loadsong(songs [songIndex]);
+
+// 更新歌曲的細節
+
+function loadsong(song){
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`
+    cover.src = `img/${song}.jpg`
+}
