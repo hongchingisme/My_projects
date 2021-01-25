f5b3e0f6f5f95cfd4efbee02d1f8da6f0328caf6
hongchingisme <hongchingisme@gmail.com>
2021-01-25 20:45:39 +0800

    add click function can play nad pause music

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -28,3 +28,33 @@ function loadsong(song){
     audio.src = `music/${song}.mp3`
     cover.src = `img/${song}.jpg`
 }
+
+//點擊事件的函式
+// 撥放音樂
+function playSong(){
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+    
+    audio.play();
+}
+
+// 暫停音樂
+function pauseSong(){
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    audio.pause();
+}
+
+// 點擊事件監聽
+
+playBtn.addEventListener('click', function(){
+    const isplaying = musicContainer.classList.contains('play');
+
+    if(isplaying){
+        pauseSong();
+    } else{
+        playSong();
+    }
+});
