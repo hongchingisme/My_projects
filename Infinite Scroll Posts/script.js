e5ed627ef63c4ea77b8777ce2233c57ecb32c98c
hongchingisme <hongchingisme@gmail.com>
2021-02-01 19:41:07 +0800

    add showPosts function that can get api data to DOM

--- Infinite Scroll Posts/script.js
+++ Infinite Scroll Posts/script.js
@@ -8,7 +8,7 @@ let page = 1 ;
 
 //Async 非同步，會先去撈資料，撈到資料才會執行，不像是同步 Sync 會等撈到資料才執行
 // 使用 fetch 抓取 api 後印出 json
-// await 則是可以暫停非同步函式的運行
+// await 則是可以暫停非同步函式的運行,並且等待 Promise 物件執行操作，等 Promise 解析後，並繼續此 Async 的執行
 async function getPosts(){
     const res = await fetch(
         `http://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
@@ -24,5 +24,21 @@ async function getPosts(){
 
 async function showPosts(){
     const posts = await getPosts();
-    console.log(posts);
-}
+
+    posts.forEach(function(post){
+        const postEl = document.createElement('div');
+        postEl.classList.add('post');
+        postEl.innerHTML = `
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+            <h2 class="post-title">${post.title}</h2>
+            <p class="post-body">${post.body}</p>
+        </div>
+        ` ;
+
+        postContainer.appendChild(postEl);
+
+    }
+    )}
+
+showPosts()
