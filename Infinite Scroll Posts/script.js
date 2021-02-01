d224c886e5f424e30be6c902b05db917828f22da
hongchingisme <hongchingisme@gmail.com>
2021-02-01 19:23:12 +0800

    dele post-container html and add opacity CSS to loader class and add show posts function

--- Infinite Scroll Posts/script.js
+++ Infinite Scroll Posts/script.js
@@ -7,7 +7,8 @@ let page = 1 ;
 
 
 //Async 非同步，會先去撈資料，撈到資料才會執行，不像是同步 Sync 會等撈到資料才執行
-
+// 使用 fetch 抓取 api 後印出 json
+// await 則是可以暫停非同步函式的運行
 async function getPosts(){
     const res = await fetch(
         `http://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
@@ -19,4 +20,9 @@ async function getPosts(){
     return data;
 }
 
-getPosts()
+// 將抓到的 posts 放到 DOM 
+
+async function showPosts(){
+    const posts = await getPosts();
+    console.log(posts);
+}
