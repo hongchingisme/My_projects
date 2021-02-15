33271d02f157cada5e956549c10097351a7b2f86
hongchingisme <hongchingisme@gmail.com>
2021-02-15 16:07:48 +0800

    get DOM and add game words

---
+++ Typing_Game/script.js
@@ -0,0 +1,32 @@
+const word = $('#word');
+const text = $('#text');
+const scoreEl = $('#score');
+const timeEl = $('#time');
+const endgameEl = $('#end-game-container');
+const settingsBtn = $('#settings-btn');
+const settings = $('#settings');
+const settingsForm = $('#settings-form');
+const difficultySelect = $('#difficulty');
+
+const words = [
+  'sigh',
+  'tense',
+  'airplane',
+  'ball',
+  'pies',
+  'juice',
+  'warlike',
+  'bad',
+  'north',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'quince',
+  'eight',
+  'feeble',
+  'admit',
+  'drag',
+  'loving'
+];
