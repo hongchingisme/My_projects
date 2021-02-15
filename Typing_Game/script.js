c2741cb263bf8bb91a2757cc3e931e09f0a787d0
hongchingisme <hongchingisme@gmail.com>
2021-02-15 16:13:35 +0800

    add random words function and push to DOM

--- Typing_Game/script.js
+++ Typing_Game/script.js
@@ -30,3 +30,27 @@ const words = [
   'drag',
   'loving'
 ];
+
+// 初始的文字
+let randomWord;
+// 初始的分數
+let score = 0 ;
+// 初始的時間
+let time = 10;
+
+// 寫入一個隨機文字的函式
+
+function getRandomWord(){
+  
+return words[Math.floor(Math.random()* words.length)];
+ 
+};
+
+// 將產生的文字顯示在 DOM 上
+
+function addWordToDom(){
+  randomWord = getRandomWord();
+  word.html(randomWord);
+};
+
+addWordToDom();
