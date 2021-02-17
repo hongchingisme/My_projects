013a3f47573aa9c275e1f158c9f52560a521c15c
hongchingisme <hongchingisme@gmail.com>
2021-02-17 21:37:28 +0800

    add the function of continuous monitoring and complete the project

--- speak_number_guess/script.js
+++ speak_number_guess/script.js
@@ -7,7 +7,7 @@ function getRandomNuber(){
     return Math.floor(Math.random()*100) +1;
 }
 
-console.log(randomNum);
+
 //設定語音辨識 api ，寫入 webkit 是為了相容性問題，有些瀏覽器不支援，所以要加入 webkit 讓他相容
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
@@ -64,4 +64,12 @@ function checkNumber(msg){
 recognition.addEventListener('result' ,onSpeak);
 
 
+recognition.addEventListener('end' , function(){
+    recognition.start();
+});
 
+document.body.addEventListener('click' , function(e){
+    if(e.target.id == "play-again"){
+        window.location.reload()
+    };
+});
