776e77ca53e66412ec35c029a135fd5e31ea9400
hongchingisme <hongchingisme@gmail.com>
2021-02-22 20:14:01 +0800

    set next year

---
+++ script.js
@@ -0,0 +1,14 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+
+//取得目前 Date 裡面的年份
+
+const currentYear = new Date().getFullYear;
+
+//設定明年 1月 1日的時間點
+
+const newYearTime = new Date(`january 01 ${currentYear +1} 00:00:00`)
+
