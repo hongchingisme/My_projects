854140dcab059f181b408c3f7b17a0f206717335
hongchingisme <hongchingisme@gmail.com>
2021-02-22 20:48:39 +0800

    add updata countdown function and pull to DOM

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear +1} 00:00:00`)
 
+// 抓取明年與現在時間的差異毫秒 (返還的值都是毫秒)
+// 透過 / 1000 取得毫秒 /60取得分鐘 /60取得小時 /24 取得天數
+function updataCountdown() {
+    const currentTime = new Date();
+    const diff  = newYearTime - currentTime ;
+    
+    const d = Math.floor(diff /1000 /60 /60 /24);
+    const h = Math.floor(diff /1000 /60 /60) %24;
+    const m = Math.floor(diff /1000 /60) %60;
+    const s = Math.floor(diff /1000) %60;
+
+    days.innerHTML= d;
+    hours.innerHTML = h < 10 ? '0'+h : h;
+    minutes.innerHTML = m< 10 ? '0'+m : m;
+    seconds.innerHTML = s< 10 ? '0'+s : s;
+};
+
+
+
+setInterval( updataCountdown , 1000);
